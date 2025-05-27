ef37e4f74bd57290424923f96866bcbcb390897e
qsj <2902385710@qq.com>
2025-05-27 15:37:16 +0800

    1

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ from lavis.processors import *
 from lavis.runners import *
 from lavis.tasks import *
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,5,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2,3,4,5,6,7"
 def parse_args():
     parser = argparse.ArgumentParser(description="Training")
 
